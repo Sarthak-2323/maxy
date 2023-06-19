2b7571a5d410c0fce8a867faa0e19ba9c39698da
Sarthak-2323 <137101467+Sarthak-2323@users.noreply.github.com>
2023-06-19 23:15:43 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://maxy-q8n0.onrender.com', {
+    const response = await fetch('https://maxy-n0az.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
